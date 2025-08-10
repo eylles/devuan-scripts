73cd1a50f49896eabcd61afcb088713c4e098cce
eylles <ed.ylles1997@gmail.com>
2025-08-10 07:29:29 -0600

    Add help output

--- fstrim.sh
+++ fstrim.sh
@@ -92,12 +92,30 @@ if [ "$#" -gt 0 ]; then
     INTERACTIVE=1
 fi
 
+show_help () {
+    printf '%s\n'   "${myname} - an fstrim helper"
+    printf '%s\n'   "Usage:"
+    printf '\t%s\n' "${myname} help | dryrun | interactive"
+    printf '\n%s\n' "This script is a helper for implementing weekly fstrim without systemd"
+    printf '%s\n'   "a quick and easy way to run this script from a cronjob, either from"
+    printf '%s\n'   "the root crontab or using anacron from cron.weekly, this is up to you."
+    printf '%s\n'   "The script will first gather a list of the filesystems that can be trimmed"
+    printf '%s\n'   "by checking that the DISC-MAX bytes are greater than 0, then it will use the"
+    printf '%s\n'   "util-linux program 'fstrim' to dispatch a trim operation to every filesystem."
+    printf '\n%s\n' "For normal operation, ie ran from a cronjob, the script will log the start of"
+    printf '%s\n'   "every fstrim operation into the cron spool with level info."
+}
+
 # input parsing
 while [ "$#" -gt 0 ]; do
     case "$1" in
         dryrun|-n|--dry-run)  DRYRUN=1  ;;
         i|interactive|-i)
             : # nothing to do, interactive mode already enabled
+            ;;
+        help|h|-h|--help)
+            show_help
+            exit 0
             ;;
         *)
             printf '%s\n' "${myname}: error, invalid argument: ${1}"
